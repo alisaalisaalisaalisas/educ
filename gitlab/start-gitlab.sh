b20071fd68a1a7c59c5973fee547a10f15e8745a
Akmal Azimov <_email@example.com>
2026-08-17 11:07:07 +0200

    dfdfdf

--- gitlab/start-gitlab.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-docker rm -f gitlab-ce 2>/dev/null || true
-docker run -d \
-  --name gitlab-ce \
-  --hostname gitlab.local \
-  -p 8090:8090 \
-  -p 2223:22 \
-  -e GITLAB_OMNIBUS_CONFIG="external_url 'http://gitlab.local:8090';" \
-  gitlab/gitlab-ce:latest
